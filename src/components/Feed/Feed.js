9b5d4cfa7a3e3f886d1a35977d6cd63643d00ab3
jameslemire <webdev.jameslemire@gmail.com>
2018-01-10 12:11:00 -0700

    JL master - In progress.

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import './Feed.css';
 
+import Post from './Post/Post';
+
 export default class Feed extends Component {
   render() {
     return (
       <section className="Feed__parent">
-
+        <Post />
       </section>
     )
   }
