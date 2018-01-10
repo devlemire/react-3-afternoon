2de7b6d41e0e5d9e4ba331d898b53441e6c24a0e
jameslemire <webdev.jameslemire@gmail.com>
2018-01-10 10:23:35 -0700

    JL master - In progress.

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import './Feed.css';
+
 export default class Feed extends Component {
   render() {
     return (
--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import './Summary.css';
+
 export default class Summary extends Component {
   render() {
     return (
